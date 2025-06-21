f256326affa3c3800e3cb79a114d78c107255948
devguy101decka <seshsnacks@gmail.com>
2025-06-21 10:58:36 -0400

    Update engine.py
    US3: Add play_turn(), running_total & current_player to GameEngine

--- src/centurion/engine.py
+++ src/centurion/engine.py
@@ -5,16 +5,26 @@ from typing import List
 class GameEngine:
     def __init__(self, counters: int = 21, include_jokers: int = 2):
         self.deck = Deck(include_jokers)
-        # Two players, each with their own hand list
         self.players: List[List[Card]] = [[], []]
         self.counters = counters
 
+        # NEW for US3:
+        self.running_total: int = 0
+        self.current_player: int = 0
+
     def start_match(self) -> None:
-        """Shuffle and deal 7 cards to each player."""
         self.deck.shuffle()
-        # Clear any existing hands
         self.players = [[], []]
-        # Deal exactly 7 cards each
         for _ in range(7):
             self.players[0].append(self.deck.deal())
             self.players[1].append(self.deck.deal())
+
+    def play_turn(self, player_index: int, card_index: int) -> int:
+        """
+        Player plays the card at card_index from their hand,
+        updates running_total, switches current_player, returns new total.
+        """
+        card = self.players[player_index].pop(card_index)
+        self.running_total += card.value()
+        self.current_player = 1 - player_index
+        return self.running_total
